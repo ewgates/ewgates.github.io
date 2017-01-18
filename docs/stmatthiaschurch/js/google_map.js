18eedfd75a174caa5bed4b1f762ed6228de36b3f
Gates <egates@galaxy.lan>
2017-01-18 15:54:59 -0500

    Added marker to map and added video

--- docs/stmatthiaschurch/js/google_map.js
+++ docs/stmatthiaschurch/js/google_map.js
@@ -2,16 +2,11 @@
 var google;
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(51.0825523,3.574402,11);
-    // 39.399872
-    // -8.224454
+    var myLatlng = new google.maps.LatLng(42.610678, -83.004438, 11);
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 15,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -30,20 +25,11 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Ghent'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
-        });
-    }
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        icon: 'images/loc.png'
+    });
     
 }
 google.maps.event.addDomListener(window, 'load', init);
